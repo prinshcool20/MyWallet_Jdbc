d79239366b4410bd73247b7ae2a295702eb20c92
prinshcool20 <30297953+prinshcool20@users.noreply.github.com>
2019-07-19 16:41:33 +0530

    Add files via upload

---
+++ WalletProject_Jdbc/src/com/cg/service/AccountOperation.java
@@ -0,0 +1,31 @@
+package com.cg.service;
+
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.cg.bean.Account;
+import com.cg.exception.InsuffecientFundException;
+
+public interface AccountOperation {
+	
+    public boolean addAccount(Account ob) throws SQLException;
+	
+	
+	
+	public boolean deleteAccount(int id) throws SQLException;
+	
+	public Account findAccount(int id) throws SQLException;
+	
+	public boolean updateAccount(int id,double amount) throws SQLException;
+	
+	
+	public ConcurrentHashMap<Long,Account> getAllAccount() throws SQLException;
+	public boolean TransferMoney(int from, int to,double amount) throws InsuffecientFundException, SQLException;
+
+
+
+	
+	
+
+}
